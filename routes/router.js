79eb66b1aea9ff7c8035b223efb7c78c3b8d397b
lucerocatalano <catalanolucero@gmail.com>
2022-08-28 17:40:17 -0300

    commit

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const controller = require('./controllers')
 
 const characters = require('./api/characters')
 const users = require('./api/users')
 
-router.use('/characters', characters);
+router.use('/characters', controller.controllerToken, characters);
 router.use('/user', users);
 
 module.exports = router;
